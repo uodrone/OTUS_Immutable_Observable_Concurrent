18100cba7f0779b979a6bda767e4adebb5b62f27
uodrone <drone_86@mail.ru>
2025-02-15 09:19:04 +0400

    Добавьте файлы проекта.

---
+++ Customer .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace OTUS_Immutable_Observable_Concurrent
+{
+    internal class Customer
+    {
+        public void OnItemChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            var col = sender as ObservableCollection<Item>;
+
+            switch (e.Action) { 
+                case NotifyCollectionChangedAction.Add:
+                    // e.NewItems содержит список добавленных элементов
+                    foreach (Item newItem in e.NewItems)
+                    {
+                        Console.WriteLine($"Добавлен товар: {newItem.Name}");
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (Item oldItem in e.OldItems)
+                    {
+                        Console.WriteLine($"Элемент удалён: {oldItem.Name}");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace OTUS_Immutable_Observable_Concurrent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Shop = new Shop();
+            var Customer = new Customer();
+
+            Shop.items.CollectionChanged += Customer.OnItemChanged;
+
+            Console.WriteLine("Нажмите A, чтобы добавить товар.");
+            Console.WriteLine("Нажмите D, чтобы удалить товар.");
+            Console.WriteLine("Нажмите X, чтобы выйти.");
+
+            while (true)
+            {
+                // Ожидаем нажатия клавиши
+                var key = Console.ReadKey(intercept: true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.A:
+                        string itemName = $"Товар от {DateTime.Now}";
+                        Shop.Add(itemName);
+                        break;
+
+                    case ConsoleKey.D:
+                        Console.WriteLine("Введите id товара для удаления: ");
+                        if (int.TryParse(Console.ReadLine(), out int id))
+                        {
+                            if (!Shop.Remove(id))
+                            {
+                                Console.WriteLine("Элемента с таким id не существует");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный идентификатор.");
+                        }
+                        break;
+
+                    case ConsoleKey.X:
+                        Console.WriteLine("Выход из программы.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Неизвестная команда.");
+                        break;
+                }
+            }
+        }
+    }
+}
